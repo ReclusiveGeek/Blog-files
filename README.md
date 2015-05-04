dc039cfe3feb3c53cfb0a8da5b445b7c78010524
Paul Jones <reclusivegeek@yahoo.co.uk>
2015-05-04 22:42:06 +0100

    Update to subs.py

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Blog-files
+
+These are files that go along with my blog to show examples
+
+
+
+program/subs.py  A simple program to change a sub title file so that it syncs to the video file correctly. 
+
+
--- programs/subs.py
+++ programs/subs.py
@@ -7,10 +7,25 @@ import datetime
 
 # need to add basic checks that the file exists
 
+if sys.argv[1] == '--help':
+    print()
+    print('Usage subs.py <file name> <millisecs>')
+    print()
+    print('   <file name>  is the name of the subtitle file and it must end have the file extension  .slt')
+    print('   <millisecs> is the number of millisecs that you want the file adjusted by. To make the subtitles appear earlier and a - to the value ie to make then appear 2 secs earlier -2000 to make then appear 2 secs later 2000')
+    print()
+    print('A new file will be created with the extension .new.slt')
+    print()
+    sys.exit()
+
+
 infile = sys.argv[1]
 adjust = int(sys.argv[2])
 
+# need to add basic checks that the file exists and what its encoded as.
+
 adjust = adjust * -1
+
 
 if infile[-3:] != 'srt':
     print('Not got the extension srt on the file ' + infile)
@@ -24,16 +39,11 @@ out = open(outfile, 'w', encoding="latin-1")
 for line in org:
     line = line.rstrip()
     if line.find('-->') > 8:
-        # print()
-        # print(line)
         temp = line.split(' --> ')
-        # tmp1 = datetime.datetime.strptime(temp[0], "%H:%M:%S,%f")- datetime.timedelta(milliseconds=adjust)
         tmp1 = datetime.datetime.strftime(datetime.datetime.strptime(temp[0], "%H:%M:%S,%f")- datetime.timedelta(milliseconds=adjust),  "%H:%M:%S,%f")[:-3]
-        # tmp2 = datetime.datetime.strptime(temp[1], "%H:%M:%S,%f") - datetime.timedelta(milliseconds=adjust)
         tmp2 = datetime.datetime.strftime(datetime.datetime.strptime(temp[1], "%H:%M:%S,%f") - datetime.timedelta(milliseconds=adjust),  "%H:%M:%S,%f")[:-3]
         new = str.join(' --> ', (tmp1, tmp2))
         out.write(new + '\n')
-        # print(str(new))
     else:
         out.write(line + '\n')
 out.close()
